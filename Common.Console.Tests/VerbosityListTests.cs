4a71b6db6f3a163840666878a8ce75a31b7f0ce3
Alex Davidson <alex.davidson@bluewire-technologies.com>
2014-10-01 11:12:41 +0100

    Add simple VerbosityList implementation.
    This functionality has been duplicated in several projects consuming this
    library.

---
+++ Common.Console.Tests/VerbosityListTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Core;
+using NUnit.Framework;
+
+namespace Bluewire.Common.Console.Tests
+{
+    [TestFixture]
+    public class VerbosityListTests
+    {
+        [Test]
+        public void DefaultVerbosityIsTheFirstEntry()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug);
+            Assert.AreEqual(Level.Warn, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void DefaultVerbosityCanBeOverridden()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug).Default(Level.Debug);
+            Assert.AreEqual(Level.Debug, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void VerbosityLevelsDoNotNeedToBeUnique()
+        {
+            new VerbosityList<Level>(Level.Warn, Level.Info, Level.Warn, Level.Debug);
+        }
+
+        [Test]
+        public void QuietSelectsAnEarlierLevelInTheList()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug).Default(Level.Info);
+            list.Quiet();
+            Assert.AreEqual(Level.Warn, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void VerboseSelectsALaterLevelInTheList()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug).Default(Level.Info);
+            list.Verbose();
+            Assert.AreEqual(Level.Debug, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void OverridingDefaultVerbosity_SelectsEarliestOccurrenceOfTheSpecifiedLevel()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Warn, Level.Debug);
+            list.Default(Level.Warn);
+            list.Verbose();
+            Assert.AreEqual(Level.Info, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void RequestingQuieterThanQuietestLevel_SelectsQuietestLevel()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug).Default(Level.Info);
+            list.Quiet();
+            list.Quiet();
+            list.Quiet();
+            Assert.AreEqual(Level.Warn, list.CurrentVerbosity);
+        }
+
+        [Test]
+        public void RequestingMoreVerboseThanMostVerboseLevel_SelectsMostVerboseLevel()
+        {
+            var list = new VerbosityList<Level>(Level.Warn, Level.Info, Level.Debug).Default(Level.Info);
+            list.Verbose();
+            list.Verbose();
+            list.Verbose();
+            Assert.AreEqual(Level.Debug, list.CurrentVerbosity);
+        }
+    }
+}
---
+++ Common.Console/VerbosityList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bluewire.Common.Console
+{
+    /// <summary>
+    /// Generic implementation of an ordered set of log verbosity levels.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class VerbosityList<T> : IEnumerable<T>
+    {
+        private readonly List<T> levels;
+
+        public VerbosityList(params T[] levelsInOrderOfIncreasingVerbosity)
+        {
+            this.levels = levelsInOrderOfIncreasingVerbosity.ToList();
+            logLevel = 0;
+        }
+
+        private int logLevel;
+
+        /// <summary>
+        /// Increase verbosity by one level.
+        /// </summary>
+        public void Verbose()
+        {
+            logLevel++;
+        }
+
+        /// <summary>
+        /// Decrease verbosity by one level.
+        /// </summary>
+        public void Quiet()
+        {
+            logLevel--;
+        }
+
+        /// <summary>
+        /// Set the default verbosity level.
+        /// </summary>
+        /// <remarks>
+        /// Unless specified using this method, the default verbosity level of the list
+        /// will be the first one, ie. the minimum.
+        /// If a level appears more than once in the list, this method will pick the first
+        /// occurrence.
+        /// </remarks>
+        /// <param name="defaultLevel"></param>
+        /// <returns></returns>
+        public VerbosityList<T> Default(T defaultLevel)
+        {
+            if (!levels.Contains(defaultLevel)) throw new ArgumentException(String.Format("Default level {0} is not in the list of available verbosity levels.", defaultLevel));
+            // If a level appears multiple times, err on the quiet side.
+            // Should not really have duplicates, though.
+            logLevel = levels.IndexOf(defaultLevel);
+            return this;
+        }
+
+        /// <summary>
+        /// Currently selected verbosity level.
+        /// </summary>
+        public T CurrentVerbosity
+        {
+            get
+            {
+                var currentLogLevel = logLevel;
+                if (currentLogLevel < 0) currentLogLevel = 0;
+                if (currentLogLevel >= levels.Count) currentLogLevel = levels.Count - 1;
+                return levels[currentLogLevel];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return levels.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

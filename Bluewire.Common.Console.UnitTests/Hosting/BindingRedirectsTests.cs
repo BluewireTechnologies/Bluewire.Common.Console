33bd49dec398b3a660c258315221d4fab29ef89c
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-03-08 21:02:40 +0000

    Inherit binding redirects from existing configuration file
    When generating a configuration file for hosting a daemon, inherit any
    binding redirects present in its configuration file.
    
    refs E-24067
    
    Bluewire.Common.Console: 9.1.0 -> 9.1.1

---
+++ Bluewire.Common.Console.UnitTests/Hosting/BindingRedirectsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Bluewire.Common.Console.Hosting;
+using NUnit.Framework;
+
+namespace Bluewire.Common.Console.UnitTests.Hosting
+{
+    [TestFixture]
+    public class BindingRedirectsTests
+    {
+        [Test]
+        public void CollectsBindingRedirectsFromConfigurationXml()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(@"
+<configuration>
+  <runtime>
+    <assemblyBinding xmlns='urn:schemas-microsoft-com:asm.v1'>
+      <dependentAssembly>
+        <assemblyIdentity name='SomeReference' publicKeyToken='79cab3b4ccd030ec' culture='neutral' />
+        <bindingRedirect oldVersion='0.0.0.0-1.2.3.4' newVersion='1.2.3.4' />
+      </dependentAssembly>
+    </assemblyBinding>
+  </runtime>
+</configuration>
+");
+
+            var redirects = BindingRedirects.ReadFrom(xml);
+
+            Assert.That(redirects, Is.Not.Null);
+            Assert.That(redirects.Count, Is.EqualTo(1));
+        }
+    }
+}

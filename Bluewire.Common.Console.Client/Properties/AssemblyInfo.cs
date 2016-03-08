00abae052aef4d32844ab13a0721ccde085bb8b1
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-03-08 22:35:46 +0000

    Use manual versioning for these libraries
    In the same manner as Bluewire.Common, it is useful to version these
    libraries individually and the CI server does not understand how to do
    this. Therefore control versioning via source code.

--- Bluewire.Common.Console.Client/Properties/AssemblyInfo.cs
+++ Bluewire.Common.Console.Client/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c66a3f12-79e2-46bd-8aab-d75414e14b6c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.1.0")]
+[assembly: AssemblyFileVersion("9.1.0")]
--- Bluewire.Common.Console.NUnit3/Properties/AssemblyInfo.cs
+++ Bluewire.Common.Console.NUnit3/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3254e82f-4b75-4984-8f08-e015da88a656")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.1.0")]
+[assembly: AssemblyFileVersion("9.1.0")]

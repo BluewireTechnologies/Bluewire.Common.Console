c4929e09cd6f768c7b65c81797bd2ef2f3c19a85
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-08-22 09:58:37 +0100

    Provide more configuration options when hosting daemons
    * Enable the 'original' configuration file to be read.
    * Provide a means of writing external configuration files at paths
      relative to the main one.
    
    Bluewire.Common.Console: 10.0.0 -> 10.1.0

--- Bluewire.Common.Console/Properties/AssemblyInfo.cs
+++ Bluewire.Common.Console/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("Bluewire.Common.Console.UnitTests")]
 
-[assembly: AssemblyVersion("10.0.0")]
-[assembly: AssemblyFileVersion("10.0.0")]
+[assembly: AssemblyVersion("10.1.0")]
+[assembly: AssemblyFileVersion("10.1.0")]

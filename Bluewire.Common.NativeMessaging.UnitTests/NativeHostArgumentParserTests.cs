da507f9885d799749b6616c5c5529bc5d41898c3
Alex Davidson <alex.davidson@bluewire-technologies.com>
2017-10-18 18:19:04 +0100

    Implement host process argument parsing

---
+++ Bluewire.Common.NativeMessaging.UnitTests/NativeHostArgumentParserTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Bluewire.Common.NativeMessaging.UnitTests
+{
+    [TestFixture]
+    public class NativeHostArgumentParserTests
+    {
+        [Test]
+        public void ParsesOriginUriAlone()
+        {
+            var sut = new NativeHostArgumentParser();
+
+            var session = sut.Parse(new [] { "chrome-extension://something" });
+
+            Assert.That(session.Origin, Is.EqualTo(new Uri("chrome-extension://something")));
+        }
+
+        [Test, Description("Chrome 54 and earlier passed this URI as the second parameter")]
+        public void ParsesOriginUriAsSecondArgument()
+        {
+            var sut = new NativeHostArgumentParser();
+
+            var session = sut.Parse(new [] { "first-arg", "chrome-extension://something" });
+
+            Assert.That(session.Origin, Is.EqualTo(new Uri("chrome-extension://something")));
+        }
+
+        [Test]
+        public void ParsesSinglePartWindowHandleArgument()
+        {
+            var sut = new NativeHostArgumentParser();
+
+            var session = sut.Parse(new [] { "--parent-window=424242" });
+
+            Assert.That(session.ParentWindowHandle, Is.EqualTo(new IntPtr(424242)));
+        }
+
+        [Test]
+        public void ParsesTwoPartWindowHandleArgument()
+        {
+            var sut = new NativeHostArgumentParser();
+
+            var session = sut.Parse(new [] { "--parent-window", "424242" });
+
+            Assert.That(session.ParentWindowHandle, Is.EqualTo(new IntPtr(424242)));
+        }
+
+        [TestCase("0.2")]
+        [TestCase("0x0dedbeef")]
+        [TestCase("0xdeadbeef")]
+        [TestCase("string")]
+        public void WhenParentWindowIsNotDecimalInteger_ThrowsFormatException(string notDecimalInteger)
+        {
+            var sut = new NativeHostArgumentParser();
+
+            Assert.Throws<FormatException>(() => sut.Parse(new [] { "--parent-window", notDecimalInteger }));
+        }
+    }
+}
---
+++ Bluewire.Common.NativeMessaging/NativeHostArgumentParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bluewire.Common.NativeMessaging
+{
+    public class NativeHostArgumentParser
+    {
+        public NativeHostSessionArguments Parse(IList<string> arguments)
+        {
+            var session = new NativeHostSessionArguments();
+
+            using (var iterator = arguments.GetEnumerator())
+            {
+                while (iterator.MoveNext())
+                {
+                    if (session.Origin == null)
+                    {
+                        Uri uri;
+                        if (Uri.TryCreate(iterator.Current, UriKind.Absolute, out uri))
+                        {
+                            session.Origin = uri;
+                        }
+                    }
+                    if (session.ParentWindowHandle == null)
+                    {
+                        int handle;
+                        if (TryParseParentWindowHandle(iterator, out handle))
+                        {
+                            session.ParentWindowHandle = new IntPtr(handle);
+                        }
+                    }
+                }
+            }
+            return session;
+        }
+
+        private static bool TryParseParentWindowHandle(IEnumerator<string> iterator, out int handle)
+        {
+            const string parentWindowArgument = "--parent-window";
+            handle = 0;
+            if (iterator.Current?.StartsWith(parentWindowArgument) != true) return false;
+
+            var remaining = iterator.Current.Substring(parentWindowArgument.Length).TrimStart('=');
+            if (!String.IsNullOrWhiteSpace(remaining))
+            {
+                if (int.TryParse(remaining, out handle)) return true;
+                throw new FormatException($"Not a decimal integer: {remaining}");
+            }
+
+            if (!iterator.MoveNext()) return false;
+            
+            if (int.TryParse(iterator.Current, out handle)) return true;
+            throw new FormatException($"Not a decimal integer: {iterator.Current}");
+        }
+    }
+
+    public class NativeHostSessionArguments
+    {
+        public Uri Origin { get; set; }
+        public IntPtr? ParentWindowHandle { get; set; }
+    }
+}

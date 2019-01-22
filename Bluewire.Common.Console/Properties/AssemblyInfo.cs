0d8979abdc42507d767b5741ded75a497a33c273
Alex Davidson <alex.davidson@bluewire-technologies.com>
2019-01-22 18:58:11 +0000

    Update to log4net v2.0.8
    Bluewire.Common.Console: 11.0.0 -> 11.1.0

--- Bluewire.Common.Console/Properties/AssemblyInfo.cs
+++ Bluewire.Common.Console/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 
 [assembly: InternalsVisibleTo("Bluewire.Common.Console.UnitTests")]
 
-[assembly: AssemblyVersion("11.0.0")]
-[assembly: AssemblyFileVersion("11.0.0")]
+[assembly: AssemblyVersion("11.1.0")]
+[assembly: AssemblyFileVersion("11.1.0")]

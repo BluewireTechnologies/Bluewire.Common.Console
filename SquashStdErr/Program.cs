8d5f358dc80c19e168a43a8080e607dfa6728c2b
Alex Davidson <alex.davidson@bluewire-technologies.com>
2020-06-29 17:35:26 +0100

    Provide a tool to squash STDERR to STDOUT
    Certain shells/hosts cannot properly handle STDERR, treating output on
    it as a fatal error instead of plain text. Redirection, etc may not work
    either.

---
+++ SquashStdErr/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using Bluewire.Common.Console.Client.Shell;
+
+namespace SquashStdErr
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var cmd = new CommandLine(args.First());
+            cmd.AddList(args.Skip(1));
+            var process = cmd.Run();
+            process.StdOut.Subscribe(Console.WriteLine);
+            process.StdErr.Subscribe(Console.WriteLine);
+            return process.Completed.Result;
+        }
+    }
+}
